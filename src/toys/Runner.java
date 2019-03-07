800242a932b57e832dd0e442601920694ff13ef2
ISAAC HOFFMAN <trsileneh@gmail.com>
2019-03-07 08:32:20 -0800

    20190307

--- src/toys/Runner.java
+++ src/toys/Runner.java
@@ -4,9 +4,9 @@ public class Runner {
 
 	public static void main(String[] args) {
 		ToyStore ts = new ToyStore();
-		ts.loadToys("barbie barbie bratz woody woody buzz bratz bratz bratz dinosaur");
+		ts.loadToys("barbie barbie bratz woody ball ball woody buzz bratz toy bratz bratz woody dinosaur");
 		System.out.println(ts);
-		System.out.println(ts.getMostFrequentToy());
+		System.out.println("most frequent toy: " + ts.getMostFrequentToy().getName());
 		ts.sortToysByCount();
 		System.out.println(ts);
 	}
